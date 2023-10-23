9046f67d1a83bb07e83f045155fef06a8b5bdadf
ZhaoKe <1355449245@qq.com>
2023-10-23 21:49:52 +0800

    rewrite

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author : ZhaoKe
 # @Time : 2021-04-07 12:05
+version = "2.0"
+SUPPORT_APP = ["FJSPAC", "Load Balance"]
+SUPPORT_ALGORITHM = ["GA", "ACO", "PSO", "CRPSO", "SA"]
