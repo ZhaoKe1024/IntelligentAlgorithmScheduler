7c186d34e2d65d36513a6e1889787c08cc3cf4c0
ZhaoKe <1355449245@qq.com>
2023-11-23 22:00:27 +0800

    add some base class

--- deepRL/DQNTrainer.py
+++ deepRL/DQNTrainer.py
@@ -12,6 +12,7 @@ from collections import deque
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
+from BaseTrainer import BaseTrainer
 from deepRL.env_knapsack import KnapsackEnv
 from deepRL.DQNModules import ReplayMemory, PrioritizedMemory
 from deepRL.DQNetwork import DQN
@@ -27,12 +28,10 @@ def setup_seed():
     torch.cuda.manual_seed(SEED)
 
 
-class DQNTrainer(object):
+class DQNTrainer(BaseTrainer):
     def __init__(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        super().__init__()
         self.env = KnapsackEnv()
-        self.policy_net = None
-        self.target_net = None
         self.score_history = []
         self.EPISODES = 1000
         self.EPS_START = 0.9  # 0.7527517282492975  # 0.9
@@ -68,31 +67,6 @@ class DQNTrainer(object):
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
         self.optimizer = optim.RMSprop(self.policy_net.parameters(), self.LR)
-
-    def __save_checkpoint(self, save_model_path, epoch_id, best_model=False):
-        policy_state_dict = self.policy_net.state_dict()
-        target_state_dict = self.target_net.state_dict()
-        os.makedirs(save_model_path, exist_ok=True)
-        if best_model:
-            policy_model_path = os.path.join(save_model_path, f"policynet_bestmodel.pth").replace('\\', '/')
-            target_model_path = os.path.join(save_model_path, f"targetnet_bestmodel.pth").replace('\\', '/')
-        else:
-            policy_model_path = os.path.join(save_model_path, f"policynet_epoch{epoch_id}.pth").replace('\\', '/')
-            target_model_path = os.path.join(save_model_path, f"targetnet_epoch{epoch_id}.pth").replace('\\', '/')
-        torch.save(policy_state_dict, policy_model_path)
-        torch.save(target_state_dict, target_model_path)
-        torch.save(self.optimizer.state_dict(), os.path.join(save_model_path, "optimizer.pth").replace('\\', '/'))
-
-    def __load_checkpoint(self, save_model_path):
-        last_epoch = -1
-        assert os.path.exists(os.path.join(save_model_path, "optimizer.pth").replace('\\','/')), f"{save_model_path}/optimizer.pth not found!"
-        assert os.path.exists(os.path.join(save_model_path, "policynet_bestmodel.pth").replace('\\','/')), "polictnet_bestmodel.pth not found!"
-        assert os.path.exists(os.path.join(save_model_path, "targetnet_bestmodel.pth").replace('\\','/')), "targetnet_bestmodel.pth not found!"
-        self.policy_net.load_state_dict(torch.load(os.path.join(save_model_path, "policynet_bestmodel.pth").replace('\\','/')))
-        self.target_net.load_state_dict(torch.load(os.path.join(save_model_path, "targetnet_bestmodel.pth").replace('\\','/')))
-        self.optimizer.load_state_dict(torch.load(os.path.join(save_model_path, "optimizer.pth").replace('\\','/')))
-        self.optimizer.step()
-        [self.optimizer.step() for _ in range(last_epoch)]  # 这里存疑，不知道需要多少步骤
 
     def learn(self, e):
         if len(self.replay_memory) < self.BATCH_SIZE:
@@ -182,6 +156,7 @@ class DQNTrainer(object):
                     price += self.env.prices[action]
 
             if e % self.TARGET_UPDATE == 0:
+                """"""
                 self.target_net.load_state_dict(self.policy_net.state_dict())
                 print("Policy_net to Target_net")
 
--- deepRL/PPOTrainer.py
+++ deepRL/PPOTrainer.py
@@ -13,18 +13,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
+from BaseTrainer import BaseTrainer
 from env_knapsack import KnapsackEnv
 from DQNModules import ReplayMemory, PrioritizedMemory
 from A2CNetwork import ValueNetwork, PolicyNetwork
 
 
-class PPOTrainer(object):
+class PPOTrainer(BaseTrainer):
     def __init__(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        SEED = 3407
-        torch.manual_seed(SEED)
-        torch.cuda.manual_seed(SEED)
-
+        super().__init__()
         self.score_history = []  # array to store reward
 
         # hyperparameters definition
@@ -61,36 +58,6 @@ class PPOTrainer(object):
                                    node=self.node, num_layers=self.num_layers, device=self.device).to(self.device)
         self.v_optimizer = optim.Adam(self.v_net.parameters(), self.V_LR)
         self.p_optimizer = optim.Adam(self.p_net.parameters(), self.P_LR)
-
-    def __save_checkpoint(self, save_model_path, epoch_id, best_model=False):
-        policy_state_dict = self.p_net.state_dict()
-        target_state_dict = self.v_net.state_dict()
-        os.makedirs(save_model_path, exist_ok=True)
-        if best_model:
-            policy_model_path = os.path.join(save_model_path, f"policynet_bestmodel.pth").replace('\\', '/')
-            target_model_path = os.path.join(save_model_path, f"targetnet_bestmodel.pth").replace('\\', '/')
-        else:
-            policy_model_path = os.path.join(save_model_path, f"policynet_epoch{epoch_id}.pth").replace('\\', '/')
-            target_model_path = os.path.join(save_model_path, f"targetnet_epoch{epoch_id}.pth").replace('\\', '/')
-        torch.save(policy_state_dict, policy_model_path)
-        torch.save(target_state_dict, target_model_path)
-        torch.save(self.optimizer.state_dict(), os.path.join(save_model_path, "optimizer.pth").replace('\\', '/'))
-
-    def __load_checkpoint(self, save_model_path):
-        last_epoch = -1
-        assert os.path.exists(os.path.join(save_model_path, "optimizer.pth").replace('\\',
-                                                                                     '/')), f"{save_model_path}/optimizer.pth not found!"
-        assert os.path.exists(os.path.join(save_model_path, "policynet_bestmodel.pth").replace('\\',
-                                                                                               '/')), "polictnet_bestmodel.pth not found!"
-        assert os.path.exists(os.path.join(save_model_path, "targetnet_bestmodel.pth").replace('\\',
-                                                                                               '/')), "targetnet_bestmodel.pth not found!"
-        self.policy_net.load_state_dict(
-            torch.load(os.path.join(save_model_path, "policynet_bestmodel.pth").replace('\\', '/')))
-        self.target_net.load_state_dict(
-            torch.load(os.path.join(save_model_path, "targetnet_bestmodel.pth").replace('\\', '/')))
-        self.optimizer.load_state_dict(torch.load(os.path.join(save_model_path, "optimizer.pth").replace('\\', '/')))
-        self.optimizer.step()
-        [self.optimizer.step() for _ in range(last_epoch)]  # 这里存疑，不知道需要多少步骤
 
     def learn(self, e):
         batch = None
